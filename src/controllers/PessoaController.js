4bc9234b439b420ce7520e2fd76836db63919b3d
Vinicius <vroliveira.eng@gmail.com>
2025-01-03 18:15:34 -0300

    Migrando e populando o BD

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,11 @@
+class PessoaController {
+  static async pegaTodas(req, res) {
+    try {
+      // acessar o modelo
+    } catch (error) {
+      // erro
+    }
+  }
+}
+
+module.exports = PessoaController;
