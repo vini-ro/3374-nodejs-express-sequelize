35dd247e543fc7aeb8e4502c8e9bd285d4733d2a
Vinicius <vroliveira.eng@gmail.com>
2025-01-04 12:36:33 -0300

    Associando Modelos

--- src/models/matricula.js
+++ src/models/matricula.js
@@ -4,13 +4,15 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Matricula extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Matricula.belongsTo(models.Pessoa, {
+        foreignKey: 'estudante_id',
+        as: 'estudante'
+      });
+      Matricula.belongsTo(models.Curso, {
+        foreignKey: 'curso_id',
+        as: 'curso'
+      });
     }
   }
   Matricula.init({
