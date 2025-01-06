c1e7ddcfe83ee3f67aabc88dbae5dfe4b470c45b
Vinicius <vroliveira.eng@gmail.com>
2025-01-06 12:50:37 -0300

    Escopos e Validação Customizada

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -1,31 +1,69 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const isCpfValido = require('../../utls/validaCpfHelper.js');
+
+
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
     static associate(models) {
       Pessoa.hasMany(models.Curso, {
-        foreignKey: 'docente_id'
+        foreignKey: 'docente_id',
       });
       Pessoa.hasMany(models.Matricula, {
         foreignKey: 'estudante_id',
+        // scope: { status: 'matriculado' },
         scope: { status: 'matriculado' },
-        as: 'aulasMatriculadas'
+        as: 'aulasMatriculadas',
       });
     }
   }
-  Pessoa.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cpf: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoa',
-    tableName: 'pessoas',
-    paranoid: true,
-  });
+  Pessoa.init(
+    {
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [3, 30],
+            msg: 'O campo nome deve ter entre 3 e 30 caracteres',
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Formato do email inválido',
+          },
+        },
+      },
+      cpf: {
+        type: DataTypes.STRING,
+        validate: {
+          cpfEhValido: (cpf) => {
+            if (!isCpfValido(cpf)) throw new Error('CPF inválido');
+          }
+        }
+      },
+      ativo: DataTypes.BOOLEAN,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Pessoa',
+      tableName: 'pessoas',
+      paranoid: true,
+      defaultScope: {
+        where: {
+          ativo: true,
+        },
+      },
+      scopes: {
+        todosOsRegistros: {
+          where: {},
+        },
+      },
+    }
+  );
   return Pessoa;
 };
