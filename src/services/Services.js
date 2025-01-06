a5c4781a4d4a94d62df8de1be0f0ce85abca21e8
Vinicius <vroliveira.eng@gmail.com>
2025-01-06 15:50:13 -0300

    Funções Agregadoras

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,3 +1,4 @@
+const { where } = require('sequelize');
 const dataSource = require('../database/models');
 
 class Services {
@@ -5,27 +6,39 @@ class Services {
     this.model = nomeDoModel;
   }
 
-  async pegaTodosOsRegistros () {
+  async pegaTodosOsRegistros(where = {}) {
     return dataSource[this.model].findAll();
   }
 
-  async pegaRegistrosPorEscopo (escopo) {
-    return dataSource[this.model].scope(escopo).findAll();
+  async pegaRegistrosPorEscopo(escopo) {
+    return dataSource[this.model]
+      .scope(escopo)
+      .findAll({ where: { ...where } });
   }
 
   async pegaUmRegistro(where) {
     return dataSource[this.model].findOne({ where: { ...where } });
   }
 
-  
+  async pegaEContaRegistros(where) {
+    return dataSource[this.model].findAndCountAll({ 
+      where: { ...where },
+      limit: 2,
+      order: [['id', 'ASC']],
+    });
+  }
+
   async criaRegistro(dadosDoRegistro) {
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
   async atualizaRegistro(dadosAtualizados, where) {
-    const listadeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
-      where: { ...where }
-    });
+    const listadeRegistrosAtualizados = dataSource[this.model].update(
+      dadosAtualizados,
+      {
+        where: { ...where },
+      }
+    );
     if (listadeRegistrosAtualizados[0] === 0) {
       return false;
     }
